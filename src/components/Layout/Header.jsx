09790c40e6dfd1e62ea33b2dc7d5a1df984a8c7f
Tynychbek <tkursanaliuulu01@gmail.com>
2022-08-06 09:11:26 +0300

    git init header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import headerImg from "../../assets/meals.jpeg";
+import classes from "./Header/Header.module.css";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <h1></h1>
+        <button></button>
+      </header>
+      <div>
+        <img src={classes["meals.jpeg"]} alt="food" />
+      </div>
+    </>
+  );
+};
